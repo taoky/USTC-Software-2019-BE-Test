002ffaac3598c2a6a5a5776b6ce59134d294709c
myl7 <myl.ustc@gmail.com>
2019-06-10 16:19:01 +0800

    Add all available urls to accounts.urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'accounts'
 urlpatterns = [
+    path('login/', views.backend_login, name='b_login'),
+    path('logout/', views.backend_logout, name='b_logout'),
+    path('register/', views.backend_register, name='b_register'),
+    path('profile/show/', views.backend_profile_show, name='b_profile_show'),
+    path('profile/edit/', views.backend_profile_edit, name='b_profile_edit'),
 ]
