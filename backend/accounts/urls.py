1acba2033c5426e75f6d03eddfc9a7a7b5d18343
myl7 <myl.ustc@gmail.com>
2019-06-11 13:01:18 +0800

    Redesign request and response api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', views.backend_login, name='b_login'),
-    path('logout/', views.backend_logout, name='b_logout'),
-    path('register/', views.backend_register, name='b_register'),
-    path('profile/show/', views.backend_profile_show, name='b_profile_show'),
-    path('profile/edit/', views.backend_profile_edit, name='b_profile_edit'),
+    path('login/', views.backend_login, name='login'),
+    path('logout/', views.backend_logout, name='logout'),
+    path('register/', views.backend_register, name='register'),
+    path('profile/', views.backend_profile, name='profile'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,123 +1,99 @@
 import json
-import hashlib
 from django.shortcuts import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
 class JsonHttpResponse(HttpResponse):
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, status_code, data={}, *args, **kwargs):
+        """Returned object. To return json as response to the client.
+
+        `status_code`: Int following RESTful rules
+        `data`: Should be a dict to be converted to json as response
+        """
         content = json.dumps(data)
+        kwargs['status'] = status_code
         kwargs['content_type'] = 'application/json'
+        kwargs['charset'] = 'utf-8'
         super(JsonHttpResponse, self).__init__(content, *args, **kwargs)
 
 
 def backend_login(request):
-    """Use `username` and `password` in request.POST to login"""
+    """Use `username` and `password` in request.POST['login_info'] to login"""
     login_info = {
-        'username': request.POST['username'],
-        'password': request.POST['password']
+        'username': request.POST['login_info']['username'],
+        'password': request.POST['login_info']['password']
     }
     current_user = authenticate(**login_info)
     if current_user is not None:
         login(request, current_user)
-        response = {
-            'status_code': 200,
-            'message': 'success'
-        }
+        return JsonHttpResponse(200)
     else:
-        response = {
-            'status_code': 400,
+        error_info = {
+            'error_code': 401001,
             'message': 'username or password error'
         }
-    return JsonHttpResponse(response)
+        return JsonHttpResponse(401, error_info)
 
 
 def backend_logout(request):
-    if request.user.is_authenticated:
-        logout(request)
-        response = {
-            'status_code': 200,
-            'message': 'success'
-        }
-    else:
-        response = {
-            'status_code': 400,
-            'message': 'not logged in yet'
-        }
-    return JsonHttpResponse(response)
+    logout(request)
+    return JsonHttpResponse(200)
 
 
 def backend_register(request):
-    """Use `username`, `password` and `email` in request.POST to register"""
+    """Use `username`, `password` and `email` in request.POST['register_info']
+    to register"""
     register_info = {
-        'username': request.POST['username'],
-        'email': request.POST['email'],
-        'password': request.POST['password']
+        'username': request.POST['register_info']['username'],
+        'email': request.POST['register_info']['email'],
+        'password': request.POST['register_info']['password']
     }
-    if request.user.is_authenticated:
-        response = {
-            'status_code': 400,
-            'message': 'have logged in'
-        }
-    elif User.objects.filter(username=register_info['username']).count():
-        response = {
-            'status_code': 400,
+    if User.objects.filter(username=register_info['username']).count():
+        error_info = {
+            'error_code': 409001,
             'message': 'duplicate username'
         }
-    else:
-        User.objects.create_user(**register_info)
-        response = {
-            'status_code': 200,
-            'message': 'success'
-        }
-    return JsonHttpResponse(response)
+        return JsonHttpResponse(409, error_info)
+    User.objects.create_user(**register_info)
+    return JsonHttpResponse(201)
 
 
-def backend_profile_show(request):
-    """Show information of current user. Results are in response.content"""
+def backend_profile(request):
+    """Show or edit the information of current user. The method is gained from
+    request.POST['profile']['method'].
+
+    If the method is `show`, return the information with putting it into
+    response.profile
+    If the method is `edit`, Use the new information in
+    request.POST['profile']['new_profile'] to edit. If the information is not
+    complete, the rest will be kept back.
+    """
     if not request.user.is_authenticated:
-        response = {
-            'status_code': 400,
-            'message': 'not logged in yet'
-        }
-    else:
+        return JsonHttpResponse(401)
+    if request.POST['profile']['method'] == 'show':
         user_plain_attr = ('username', 'email', 'first_name', 'last_name')
         user_info = {
             attr: getattr(request.user, attr) for attr in user_plain_attr
         }
         user_info['password'] = 'currently not return this'
-        response = {
-            'status_code': 200,
-            'message': 'success',
-            'content': user_info
-        }
-    return JsonHttpResponse(response)
-
-
-def backend_profile_edit(request):
-    """Use attribute names and values in request.POST to edit user profile"""
-    if not request.user.is_authenticated:
-        response = {
-            'status_code': 400,
-            'message': 'not logged in yet'
-        }
-    else:
-        if 'password' in request.POST:
-            if request.user.check_password(request.POST['password']):
-                request.user.set_password(request.POST['password'])
+        return JsonHttpResponse(200, user_info)
+    elif request.POST['profile']['method'] == 'edit':
+        if 'password' in request.POST['profile']['new_profile'].keys():
+            new_password = ['profile']['new_profile']['password']
+            if request.user.check_password(new_password):
+                request.user.set_password(new_password)
             else:
-                response = {
-                    'status_code': 400,
+                error_info = {
+                    'error_code': 400001,
                     'message': 'invailed password'
                 }
-                return JsonHttpResponse(response)
+                return JsonHttpResponse(400, error_info)
         user_plain_editable_attr = ('email', 'first_name', 'last_name')
         for attr in user_plain_editable_attr:
-            if attr in request.POST:
-                setattr(request.user, attr, request.POST[attr])
-        response = {
-            'status_code': 200,
-            'message': 'success'
-        }
-    return JsonHttpResponse(response)
+            if attr in request.POST['profile']['new_profile'].keys():
+                setattr(request.user, attr,
+                        request.POST['profile']['new_profile'][attr])
+        return JsonHttpResponse(200)
+    else:
+        return JsonHttpResponse(204)
