4add3a1bf123061da99562d5bc44872ad05d2bf9
Elsa <zeyugao@outlook.com>
2019-06-15 14:44:24 +0800

    Migrate to AbstractUser-based user model Add app 'accounts' in settings

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+admin.register(UserAdmin, User)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
