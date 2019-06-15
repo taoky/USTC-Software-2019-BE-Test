8c3d704ce45d6f32dde416f2b2b9feebf6236803
myl7 <myl.ustc@gmail.com>
2019-06-16 01:51:35 +0800

    Add message models

--- backend/message/models.py
+++ backend/message/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Message(models.Model):
+    sent_time = models.DateTimeField(default=timezone.now())
+    hidden_time = models.IntegerField(default=0)  # Hidden seconds
+    content = models.CharField(max_length=255)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    reciever = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def conveyed_time(self):
+        return self.send_time + timezone.timedelta(seconds=self.hidden_time)
