530d3bf2f18e5688362bcb8584bd406ec1f6f91b
LiuShige123 <liushige@mail.ustc.edu.cn>
2019-06-20 16:35:01 +0800

    add two models: UserForm and LoginForm  and modify the views:login and register

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django import forms
 
-class User(models.Model):
-    gender = (
+gender = (
         ('male','男'),
         ('female','女'),
-        ('unknown','不明'),
+        ('unknown','未知'),
     )
-    name = models.CharField(max_length=30,unique=True)
+
+class User(models.Model):
+    name = models.CharField(max_length=100,unique=True)
     password = models.CharField(max_length=16)
     sex = models.CharField(max_length=10,choices=gender,default='male')
     email = models.CharField(max_length=256,blank=True)
@@ -14,6 +16,17 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+class UserForm(forms.Form):
+    
+    name = forms.CharField(max_length=100,label='name',strip=True)
+    password = forms.CharField(max_length=16,label='password')
+    gender = forms.ChoiceField(choices=gender,label='sex')
+    email = forms.EmailField(label='email')
+
+class LoginForm(forms.Form):
+
+    name = forms.CharField(max_length=100,label='name',strip=True)
+    password = forms.CharField(max_length=16,label='password')
 
 
 # Create your models here.
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+from .models import UserForm, LoginForm, User
 
 
 def index(request):
     return HttpResponse("You are in the index")
 
 def login(request):
-    return HttpResponse('You are in the login')
+    if request.method == 'POST':
+        userform = LoginForm(request.POST)
+        if userform.is_valid():
+            username = userform.cleaned_data['username']
+            password = userform.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = 'incorrect password'
+            except:
+                massage="The user doesn't exist"
+        return redirect('/login/')
+    else:
+        return HttpResponse('You are in the login')
+        
 
 def register(request):
-    return HttpResponse('You are in the register')
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            user = User()
+            user.name = userform.cleaned_data['username']
+            user.password = userform.cleaed_data['password']
+            user.gender = userform['gender']
+            user.email = userform['email']
+            massage = 'registered successfully'
+    else:
+        return HttpResponse('You are in the register')
 
 def logout(request):
     return HttpResponse('You are in the logout')
