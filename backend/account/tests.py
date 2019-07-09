e6e806ded62ea4a60fc62be31fa3815341a2386f
LiuShige123 <liushige@mail.ustc.edu.cn>
2019-07-09 23:36:17 +0800

    change and add the tests so that the post method of the request can be tested,simplize some of the names in views

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -19,24 +19,54 @@ class AccountTests(TestCase):
         self.client = Client()
         response=self.client.get('http://127.0.0.1:8000/account/')
         self.assertEqual(response.status_code,200)
+        self.assertEqual(json.loads(response.content)["err_code"],"000")
         
     def test_login(self):
         
         response=self.client.get('http://127.0.0.1:8000/account/login')
         self.assertEqual(response.status_code,200)
     
-      #  data={"username":"bill","password":"123456"}
-      #  response=self.client.post('http://127.0.0.1:8000/account/login',data,content_type="application/json")
-      #  self.assertEqual(response.status_code,200)
+        data={"username":"bill","password":"123456"}
+        response=self.client.post('http://127.0.0.1:8000/account/login',data)
+        self.assertEqual(response.status_code,200)
+
+        data={"username":"bill","password":"12345"}
+        response=self.client.post('http://127.0.0.1:8000/account/login',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"102")
+
+        data={"username":"david","password":"123456"}
+        response=self.client.post('http://127.0.0.1:8000/account/login',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"103")
+
 
     def test_register(self):
 
         response=self.client.get('http://127.0.0.1:8000/account/register')
         self.assertEqual(response.status_code,200)
 
-        
+        data={"username":"david",
+        "password":"23456",
+        "repassword":"23456",
+        "sex":"male",
+        "email":"12345@qq.com"}
+        response=self.client.post('http://127.0.0.1:8000/account/register',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"201")
+
+        data={"username":"david",
+        "password":"23456",
+        "repassword":"123456",
+        "sex":"male",
+        "email":"12345@qq.com"}
+        response=self.client.post('http://127.0.0.1:8000/account/register',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"203")
+
+ 
     
     def test_logout(self):
+        
+        session = self.client.session
+        session['username'] = 'bill'
+        session.save()
 
         response=self.client.get('http://127.0.0.1:8000/account/logout')
         self.assertEqual(response.status_code,200)
@@ -52,6 +82,7 @@ class AccountTests(TestCase):
         session = self.client.session
         session['username'] = 'bill'
         session.save()
+
         response=self.client.get('http://127.0.0.1:8000/account/user_index')
         self.assertEqual(json.loads(response.content)["err_code"],"400")
         self.test_logout()
@@ -60,7 +91,7 @@ class AccountTests(TestCase):
 
         response=self.client.get('http://127.0.0.1:8000/account/update_user_index')
         self.assertEqual(response.status_code,200)
-        self.assertEqual(json.loads(response.content)["err_code"],"500")
+        self.assertEqual(json.loads(response.content)["err_code"],"501")
 
     
     def test_update_user_index_valid(self):
@@ -68,9 +99,26 @@ class AccountTests(TestCase):
         session = self.client.session
         session['username'] = 'bill'
         session.save()
+
         response=self.client.get('http://127.0.0.1:8000/account/update_user_index')
         self.assertEqual(response.status_code,200)
-        self.assertEqual(json.loads(response.content)["err_code"],"501")
-        self.test_logout()
+        self.assertEqual(json.loads(response.content)["err_code"],"500")
+
+        data={
+            "new_password":"23456",
+            "re_new_password":"23456",
+            "new_sex":"male",
+            "new_email":"12345@qq.com"}
+        response=self.client.post('http://127.0.0.1:8000/account/update_user_index',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"502")
+
+        data={
+            "new_password":"23456",
+            "re_new_password":"123456",
+            "new_sex":"male",
+            "new_email":"12345@qq.com"}
+        response=self.client.post('http://127.0.0.1:8000/account/update_user_index',data)
+        self.assertEqual(json.loads(response.content)["err_code"],"503")
+
 
 # Create your tests here.
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,72 +1,74 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from . import models
-from .models import RegisterForm, LoginForm, UpdateForm
+from .models import RegisterForm, LoginForm, UpdateForm,User
 import json
 from django.core import serializers
 
 def index(request):
     resp={"err_code":"000","message":"You are in the index"}
-    return HttpResponse(json.dumps(resp),content_type="application/json")
+    return JsonResponse(resp)
 
 def login(request):
     """"""
     if request.method == 'POST':
-        userform = LoginForm(request.POST)
-        if userform.is_valid():
-            username = userform.cleaned_data['username']
-            password = userform.cleaned_data['password']
+        user_form = LoginForm(request.POST)#
+        cd_data = userform.cleaned_data#request.POST.dict()
+        if user_form.is_valid():#True:
+            username = cd_data['username']
+            password = cd_data['password']
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
                     request.session['username'] = username
                     resp={"err_code":"101","message":"Login successfully"}
-                    return HttpResponse(json.dumps(resp),content_type="application/json")
+                    return JsonResponse(resp)
                 else:
                     resp={"err_code":"102","message":"Wrong password"}
-                    return HttpResponse(json.dumps(resp),content_type="application/json")
+                    return JsonResponse(resp)
             except:
                 resp={"err_code":"103","message":"User does not exist"}
-                return HttpResponse(json.dumps(resp),content_type="application/json")
+                return JsonResponse(resp)
         else:
             resp={"err_code":"104","message":"Input invalid"}
     else:
         resp={"err_code":"100","message":"You are in the login"}
-        return HttpResponse(json.dumps(resp),content_type="application/json")
+        return JsonResponse(resp)
         
 
 def register(request):
     if request.method == "POST":
-        user_form = RegisterForm(request.POST)
-        if user_form.is_valid():
-            if user_form.cleaned_data['password'] == user_form.cleaned_data['repassword']:
+        user_form = RegisterForm(request.POST)#
+        cd_data = user_form.cleaned_data#request.POST.dict()
+        if user_form.is_valid():#True:
+            if cd_data['password'] == cd_data['repassword']:
                 user = User()
-                user.name = user_form.cleaned_data['username']
-                user.password = user_form.cleaed_data['password']
-                user.sex = user_form.cleaned_data['sex']
-                user.email = user_form.cleaned_data['email']
+                user.name = cd_data['username']
+                user.password = cd_data['password']
+                user.sex = cd_data['sex']
+                user.email = cd_data['email']
                 user.save()
                 request.session['username'] = user.name
                 resp={"err_code":"201","message":"Registered seccessfully"}
-                return HttpResponse(json.dumps(resp),content_type="application/json")
+                return JsonResponse(resp) 
             else:
                 resp={"err_code":"203","message":"please input password again"}
-                return HttpResponse(json.dumps(resp),content_type="application/json")
+                return JsonResponse(resp)
         else:
             resp={"err_code":"202","message":"Input invalid"}
-            return HttpResponse(json.dumps(resp),content_type="application/json")
+            return JsonResponse(resp)
     else:
         resp={"err_code":"200","message":"You are in the register"}
-        return HttpResponse(json.dumps(resp),content_type="application/json")
+        return JsonResponse(resp)
 
 def logout(request):
     try:
         del request.session['username']
     except KeyError:
         resp={"err_code":"301","message":"session cancelled already"}
-        return HttpResponse(json.dumps(resp),content_type="application/json")
+        return JsonResponse(resp)
     resp={"err_code":"300","message":"Logout successfully"}
-    return HttpResponse(json.dumps(resp),content_type="application/json")
+    return JsonResponse(resp)
 
 def user_index(request):
     """the information of the user which is identified through session"""
@@ -74,10 +76,10 @@ def user_index(request):
         username = request.session['username']
     except KeyError:
         resp={"err_code":"401","message":"Permission denied"}
-        return HttpResponse(json.dumps(resp),content_type="application/json")
+        return JsonResponse(resp)
     user = models.User.objects.get(name=username)
     resp = {"err_code":"400",'username':user.name,'password':user.password,'sex':user.sex,'email':user.email}
-    return HttpResponse(json.dumps(resp),content_type="application/json")
+    return JsonResponse(resp)
 
 def update_user_index(request):
 
@@ -86,33 +88,34 @@ def update_user_index(request):
             username = request.session['username']
         except KeyError:
             resp={"err_code":"501","message":"Permission denied"}
-            return HttpResponse(json.dumps(resp),content_type="application/json")
-        update_user = models.User.objects.get(name=username)
+            return JsonResponse(resp)
+        u_user = models.User.objects.get(name=username)
 
-        update_form = UpdateForm(request.POST)
-        if update_form.is_valid:
-            if update_form.cleaned_data['re_new_password'] == update_form.cleaned_data['new_password']:
-                update_user.password = update_form.cleaned_data['new_password']
-                update_user.sex = update_form.cleaned_data['new_sex']
-                update_user.email = update_form.cleaned_data['new_email']
-                update_user.is_active=True
-                update_user.save()
+        update_form = UpdateForm(request.POST)#
+        cd_data = update_form.cleaned_data#request.POST.dict()
+        if update_form.is_valid:#True
+            if cd_data['re_new_password'] == cd_data['new_password']:
+                u_user.password = cd_data['new_password']
+                u_user.sex = cd_data['new_sex']
+                u_user.email = cd_data['new_email']
+                u_user.is_active=True
+                u_user.save()
                 resp={"err_code":"502","message":"Updated successfully"}
-                return HttpResponse(json.dumps(resp),content_type="application/json")
+                return JsonResponse(resp)
             else:
                 resp={"err_code":"503","message":"Please input password again"}
-                return HttpResponse(json.dumps(resp),content_type="application/json")
+                return JsonResponse(resp)
         else:
             resp={"err_code":"504","message":"Invalid input"}
-            return HttpResponse(json.dumps(resp),content_type="application/json")
+            return JsonResponse(resp)
     else:
         try:
             username = request.session['username']
         except KeyError:
             resp={"err_code":"501","message":"Permission denied"}
-            return HttpResponse(json.dumps(resp),content_type="application/json")
+            return JsonResponse(resp)
         resp={"err_code":"500","message":"You are in the update_user_index"}
-        return HttpResponse(json.dumps(resp),content_type="application/json")
+        return JsonResponse(resp)
 
 
     
