e45378cfb6f1fbe6ebae8dfba42eec8ed40cb7d9
kaleid-liner <noob@mail.ustc.edu.cn>
2019-07-09 23:15:59 +0800

    beautify

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -98,10 +98,12 @@ def edit_view(request, username):
     else:
         return JsonResponse(ret_json)
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, created, **kwargs):
