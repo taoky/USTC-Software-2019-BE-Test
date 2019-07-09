8ffa6c463b1d89237aa9cef5f34bdb23a142ead2
kaleid-liner <noob@mail.ustc.edu.cn>
2019-07-09 20:42:37 +0800

    account: fix bug: add missing else

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'account'
 urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
