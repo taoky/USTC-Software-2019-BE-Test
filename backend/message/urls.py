0d2163e3541adad84cb5d9bc063a5b851783a29c
kaleid-liner <noob@mail.ustc.edu.cn>
2019-07-10 19:12:45 +0800

    Change message url for consistency

--- backend/message/urls.py
+++ backend/message/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'message'
 urlpatterns = [
-    path('index/<slug:username>', views.index_view, name='index'),
-    path('create/<slug:username>', views.create_view, name='create'),
+    path('<slug:username>/', views.index_view, name='index'),
+    path('<slug:username>/create', views.create_view, name='create'),
 ]
